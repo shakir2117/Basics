af62be04019493ca2cb6e82a79ff4d74b720b4e0
shakir <shakirchoudhary2117@gmail.com>
2023-01-05 17:56:06 +0530

    matrix practice

--- dict.py
+++ dict.py
@@ -27,7 +27,24 @@ dict['abc']=100
 print(f"the dictionary after the addtion of key{dict} ")
 
 #merge dict
+def merge(dict,dict2):
+    return(dict2.update(dict)) # Python code to merge dict using update() method
 
+dict2={
+    'lmn':500,
+    'opq':300,
+    'ers':200
+}
 
+merge(dict,dict2)
+print(dict2)
 
-
+# clear() – Remove all the elements from the dictionary
+# copy() – Returns a copy of the dictionary
+# get() – Returns the value of specified key
+# items() – Returns a list containing a tuple for each key value pair
+# keys() – Returns a list containing dictionary’s keys
+# pop() – Remove the element with specified key
+# popitem() – Removes the last inserted key-value pair
+# update() – Updates dictionary with specified key-value pairs
+# values() – Returns a list of all the values of dictionary
